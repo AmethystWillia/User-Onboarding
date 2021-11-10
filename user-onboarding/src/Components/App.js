9e3b1fbb8e57844690f6603c950965a40acdd393
Amethyst Williams <zmithic@gmail.com>
2021-11-10 15:51:59 -0600

    Set up the App.js

---
+++ user-onboarding/src/Components/App.js
@@ -0,0 +1,123 @@
+// Import dependencies
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+
+// Import CSS
+import './App.css';
+
+// Import components
+import schema from '../Validation/formSchema';
+import User from './User';
+import UserForm from './UserForm';
+
+//----------------  Setting Initial States  ----------------//
+// Set initial form values
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  terms: false,
+};
+// Set initial form error values
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  terms: '',
+};
+// Set initial array of users
+const initialUsers = [];
+// Set initial boolean for submit to be disabled
+const initialDisabled = true;
+
+// Exported component
+function App() {
+  //----------------  Setting States  ----------------//
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  //----------------  Creating Helpers  ----------------//
+  // Grab users from api and set them to `users`
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(`Ruh roh Raggy! ${err}`);
+      })
+  };
+
+  // Post new users to API
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(`Ruh roh Raggy! ${err}`);
+      })
+      .finally(() => {
+        setFormValues(initialFormValues);
+      })
+  };
+
+  // Check the form to see if the inputs are valid
+  const validate = (name, value) => {
+    yup.reach(schema, name).validate(value)
+      .then(() => {
+        setFormErrors({ ...formErrors, [name]: '' });
+      })
+      .catch(err => {
+        setFormErrors({ ...formErrors, [name]: err.errors[0] });
+      })
+  };
+
+  //----------------  Event Handlers  ----------------//
+  // Input-change event handler
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  };
+
+  // Submit event handler
+  const formSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password,
+      terms: formValues.terms,
+    }
+
+    postNewUser(newUser);
+  };
+
+    //----------------  Side Effects  ----------------//
+    // Acquire users
+    useEffect(() => {
+      getUsers();
+    }, []);
+
+    // Adjust value of `disabled` when `formValues` changes
+    useEffect(() => {
+      schema.isValid(formValues)
+        .then(valid => {
+          setDisabled(!valid);
+        })
+    }, [formValues]);
+
+  // Render content
+  return (
+    <div className="App">
+      <h1>Bruh</h1>
+    </div>
+  );
+}
+
+export default App;
